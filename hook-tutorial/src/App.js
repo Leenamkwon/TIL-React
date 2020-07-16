372ba2c3a5a11066fd1bab288c046fd062d84620
Leenamkwon <namkwon12@gmail.com>
2020-07-16 12:37:14 +0900

    hooks learning

---
+++ hook-tutorial/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import Counter from './Counter';
+// import Info from './info';
+import Average from './average';
+
+function App() {
+  return <Average></Average>;
+}
+
+export default App;
