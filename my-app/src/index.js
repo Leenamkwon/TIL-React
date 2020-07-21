66e5ab8fefd052db86feebeb67ed1d49b4aa5cce
Leenamkwon <namkwon12@gmail.com>
2020-07-21 19:56:10 +0900

    Life cyle method

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+	state = { lat: null, long: null, errorMessage: '' };
 
-  return <div>hello</div>;
-};
+	componentDidMount() {
+		console.log('My component was rendered to the screen');
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({
+					lat: position.coords.latitude,
+					long: position.coords.longitude,
+				});
+			},
+			(err) => this.setState({ errorMessage: err.message })
+		);
+	}
+
+	componentDidUpdate() {
+		console.log('My component was just updated = it rerender');
+	}
+
+	renderContent() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>;
+		}
+
+		return (
+			<div>
+				<Spinner message='Please accept location request'></Spinner>
+			</div>
+		);
+	}
+
+	render() {
+		return <div>{this.renderContent()}</div>;
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
