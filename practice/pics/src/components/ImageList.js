92623649049779b82485fa42b53c2c2da07aabe7
Leenamkwon <namkwon12@gmail.com>
2020-07-22 17:48:44 +0900

    image app

--- practice/pics/src/components/ImageList.js
+++ practice/pics/src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImagesList = (props) => {
-	const images = props.images.map((image, index) => {
-		return <img key={index} src={image.urls.small} alt='' />;
+	const images = props.images.map((image) => {
+		return <ImageCard key={image.id} image={image} />;
 	});
 
-	return <div>{images}</div>;
+	return <div className='image-list'>{images}</div>;
 };
 
 export default ImagesList;
