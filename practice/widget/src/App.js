cf7901313ec982a39655120ba8f0fbea9bb55333
Leenamkwon <namkwon12@gmail.com>
2020-07-24 02:54:56 +0900

    upgrade widget

--- practice/widget/src/App.js
+++ practice/widget/src/App.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
-import './components/Accordion.css';
+import Search from './components/Search';
 
 const items = [
 	{
 		title: 'What is React?',
 		content: 'React is a front end javascript framework',
-		id: 0,
 	},
 	{
 		title: 'Why use React?',
 		content: 'React is a favorite JS ',
-		id: 1,
 	},
 	{
 		title: 'How do you use React?',
 		content: 'You use React by creating components',
-		id: 2,
+	},
+];
+
+const list2 = [
+	{
+		title: 'accordion',
+	},
+	{
+		title: 'sex',
+	},
+	{
+		title: 'bombar',
 	},
 ];
 
 export default () => {
+	const [widget, setWidget] = useState(null);
+	const [dom, setdom] = useState(null);
+
+	const render = (title) => {
+		setWidget(title);
+
+		if (widget === title) {
+			return setdom(<Accordion items={items} />);
+		} else {
+			setdom(null);
+		}
+	};
+
+	const list = list2.map((item, index) => (
+		<span
+			onClick={() => render(item.title)}
+			key={item.title}
+			style={{cursor: 'pointer', margin: '15px', padding: '100px'}}
+		>
+			{item.title}
+		</span>
+	));
+
 	return (
 		<div>
-			<Accordion items={items}></Accordion>
+			{list}
+			{dom}
+			<Search></Search>
 		</div>
 	);
 };
