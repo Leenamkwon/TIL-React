ad6e62f2c466b9169e5afbc767bed81084f4c627
Leenamkwon <namkwon12@gmail.com>
2020-07-25 17:03:29 +0900

    widget for wiki search api

--- practice/widget/src/components/Search.js
+++ practice/widget/src/components/Search.js
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
-	return <h1>Search</h1>;
+	const [term, setTerm] = useState('');
+	const [results, setResults] = useState([]);
+
+	useEffect(() => {
+		const search = async () => {
+			const {data} = await axios.get(
+				'https://en.wikipedia.org/w/api.php',
+				{
+					params: {
+						action: 'query',
+						list: 'search',
+						origin: '*',
+						format: 'json',
+						srsearch: term,
+					},
+				}
+			);
+
+			setResults(data.query.search);
+		};
+		if (term) {
+			search();
+		}
+	}, [term]);
+
+	const renderedResults = results.map((results) => {
+		return (
+			<div className='item' key={results.pageid}>
+				<div className='right floated content'>
+					<a
+						className='ui button'
+						href={`https://en.wikipedia.org?curid=${results.pageid}`}
+					>
+						Go
+					</a>
+				</div>
+				<div className='content'>
+					<div className='header'>{results.title}</div>
+					<span
+						dangerouslySetInnerHTML={{__html: results.snippet}}
+					></span>
+				</div>
+			</div>
+		);
+	});
+
+	return (
+		<div>
+			<div className='ui form'>
+				<div className='field'>
+					<label>Enter Search Term</label>
+					<input
+						type='text'
+						className='input'
+						value={term}
+						onChange={(e) => setTerm(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className='ui celled list'>{renderedResults}</div>
+		</div>
+	);
 };
 
 export default Search;
