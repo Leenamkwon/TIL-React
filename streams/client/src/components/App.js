5408946126173ff792f20e3979fb4215437d2077
Leenamkwon <namkwon12@gmail.com>
2020-09-04 23:59:40 +0900

    router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => {
-  return (
-    <div>
-      Page one
-      <Link to='/pagetwo'>Nlinkviglinkte to page Two</Link>
-    </div>
-  );
-};
-const pageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click Me!</button>
-      <Link to='/'>Navigate to page One</Link>
-    </div>
-  );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
+      <h1>Header</h1>
       <BrowserRouter>
         <div>
-          <Route path='/' exact={true} component={PageOne} />
-          <Route path='/pagetwo' component={pageTwo} />
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' exact component={StreamCreate} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
